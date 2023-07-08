2f86313be843f3a94abae867b1412b673299b89e
namnguyen <nguyenducnam62@gmail.com>
2023-07-08 09:00:30 +0700

    test static8

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-import mimetypes
+# import mimetypes
 
-mimetypes.add_type("text/css", ".css", True)
-mimetypes.add_type("text/html", ".html", True)
-mimetypes.add_type("text/javascript", ".js", True)
+# mimetypes.add_type("text/css", ".css", True)
+# mimetypes.add_type("text/html", ".html", True)
+# mimetypes.add_type("text/javascript", ".js", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
